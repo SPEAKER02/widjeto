13a6e02f3bb2d76232ec09b2fd66667226d8e672
pablo <pablo@debian>
2024-06-26 20:30:58 +0300

    Addet basic config files

---
+++ .config/polybar/pwidgets/launch.sh
@@ -0,0 +1,92 @@
+#!/usr/bin/env sh
+
+DIR="$HOME/.config/polybar/pwidgets"
+
+kill_bar() {
+	# Terminate already running bar instances
+	killall -q polybar
+
+	# Wait until the processes have been shut down
+	while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+}
+
+## Main
+if [[ "$1" == "--main" ]]; then
+	kill_bar
+	#apps (main)
+	polybar -q primary -c $DIR/main.ini &
+	polybar -q secondary -c $DIR/main.ini &
+	polybar -q primary2 -c $DIR/main.ini &
+	polybar -q secondary2 -c $DIR/main.ini &
+	# polybar -q top -c $DIR/main.ini &
+	# Workspace
+	polybar -q primary -c $DIR/workspace.ini &
+	polybar -q primary_2 -c $DIR/workspace.ini &
+	polybar -q secondary -c $DIR/workspace.ini &i
+	polybar -q secondary_2 -c $DIR/workspace.ini &i
+	# System 
+	polybar -q primary -c $DIR/system.ini &
+	polybar -q primary2 -c $DIR/system.ini &
+	#polybar -q secondary -c $DIR/system.ini &
+	#mpd
+	#polybar -q primary -c $DIR/mpd.ini &
+	#polybar -q secondary -c $DIR/mpd.ini &
+	# Cpu
+	#polybar -q primary -c $DIR/cpu.ini &
+	#polybar -q secondary -c $DIR/cpu.ini &
+	# Memory	
+	#polybar -q primary -c $DIR/memory.ini &
+	#polybar -q secondary -c $DIR/memory.ini &
+	# Filesystem	
+	#polybar -q primary -c $DIR/fs.ini &
+	#polybar -q secondary -c $DIR/fs.ini &
+
+## Apps
+elif [[ "$1" == "--apps" ]]; then
+	kill_bar
+	polybar -q primary -c $DIR/apps.ini &
+	polybar -q secondary -c $DIR/apps.ini &
+
+## System
+elif [[ "$1" == "--system" ]]; then
+	kill_bar
+	polybar -q primary -c $DIR/system.ini &
+	polybar -q secondary -c $DIR/system.ini &
+
+## Mpd
+elif [[ "$1" == "--mpd" ]]; then
+	kill_bar
+	polybar -q primary -c $DIR/mpd.ini &
+	polybar -q secondary -c $DIR/mpd.ini &
+
+## Workspaces
+elif [[ "$1" == "--ws" ]]; then
+	kill_bar
+	polybar -q primary -c $DIR/workspace.ini &
+	polybar -q secondary -c $DIR/workspace.ini &
+
+## CPU
+elif [[ "$1" == "--cpu" ]]; then
+	kill_bar
+	polybar -q primary -c $DIR/cpu.ini &
+	polybar -q secondary -c $DIR/cpu.ini &
+
+## Memory
+elif [[ "$1" == "--memory" ]]; then
+	kill_bar
+	polybar -q primary -c $DIR/memory.ini &
+	polybar -q secondary -c $DIR/memory.ini &
+
+## Filesystem
+elif [[ "$1" == "--fs" ]]; then
+	kill_bar
+	polybar -q primary -c $DIR/fs.ini &
+	polybar -q secondary -c $DIR/fs.ini &
+
+else
+	cat <<- _EOF_
+	No widget specified, Available widgets:
+	--main    --apps    --system    --mpd
+	--ws      --cpu     --memory    --fs
+	_EOF_
+fi
